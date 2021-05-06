3dffe6953081c4b4623b5369b1a60de39daac82c
Steven Fackler <sfackler@gmail.com>
2021-05-06 19:03:29 -0400

    Update to 1.52.0

--- x.py
+++ x.py
@@ -6,8 +6,8 @@ import os
 import subprocess
 import sys
 
-rust_version = "1.51.0"
-rustup_version = "1.23.1"
+rust_version = "1.52.0"
+rustup_version = "1.24.1"
 
 DebianArch = namedtuple("DebianArch", ["bashbrew", "dpkg", "rust"])
 
