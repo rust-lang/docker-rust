bf46e4541bbb57fb6816926a593aa568c43806f0
Scott Schafer <schaferjscott@gmail.com>
2023-03-23 09:23:00 -0500

    chore: Bump to rust version 1.68.1

--- x.py
+++ x.py
@@ -6,7 +6,7 @@ import os
 import subprocess
 import sys
 
-rust_version = "1.68.0"
+rust_version = "1.68.1"
 rustup_version = "1.25.2"
 
 DebianArch = namedtuple("DebianArch", ["bashbrew", "dpkg", "rust"])
